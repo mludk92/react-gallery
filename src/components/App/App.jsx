10ded629fe07d3b08c9d28773669924f970e5595
Mitch Ludkey <mludk92@hotmail.com>
2023-04-09 17:18:34 -0500

    photos now work with flip card set up, https://www.geeksforgeeks.org/design-a-flip-card-effect-using-reactjs/ credit to this site.

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './App.css';
-
+import ReactCardFlip from "react-card-flip";
 
 function App() {
     const [gallery, setGallery] = useState([])
+    const [flippedIndex, setFlippedIndex] = useState(-1);
 
     // Axios GET Request to get photos from the server
     const getGallery = () => {
@@ -19,6 +20,9 @@ function App() {
         getGallery();
     }, []);
 
+    const handleFlip = (index) => {
+        setFlippedIndex(index === flippedIndex ? -1 : index);
+    }
 
     return (
       <div className="App">
@@ -26,14 +30,67 @@ function App() {
           <h1 className="App-title">Gallery of My Life</h1>
         </header>
         <p>Gallery goes here</p>
-        {gallery.map((photo) => (
+        {gallery.map((photo, index) => (
           <div key={photo.id}>
-            <img src={photo.path} alt={photo.title} style={{ maxWidth: '300px', maxHeight: '300px' }} />
-            <p>{photo.title}</p>
-            <p>{photo.description}</p>
-            <p>{photo.likes} likes</p>
+            <ReactCardFlip isFlipped={flippedIndex === index} 
+            flipDirection="vertical">
+            <div style={{
+                width: '250px',
+                height: '250px',
+                backgroundImage: `url(${photo.path})`,
+                backgroundSize:'cover',
+                backgroundPosition: 'center top',
+                fontSize: '40px',
+                color: 'green',
+                margin: '20px',
+                borderRadius: '4px',
+                textAlign: 'center',
+                padding: '20px'
+            }}>
+                <br />
+                <br />
+                <button style={{
+                width: '100px',
+                fontSize: '20px',
+                background: 'white',
+                fontWeight: 'bold',
+                borderRadius: '5px',
+                position: 'absolute',
+                bottom: '10px',
+                left: '30px'
+                }} onClick={() => handleFlip(index)}>
+                    Flip</button>
+            </div>
+            <div style={{
+                width: '300px',
+                height: '300px',
+                background: 'darkgray',
+                fontSize: '15px',
+                color: 'blue',
+                margin: '20px',
+                borderRadius: '4px',
+                textAlign: 'center',
+                padding: '20px'
+            }}>
+                <h1>{photo.title}</h1>
+                {photo.description}
+                <br />
+                <button style={{
+                width: '100px',
+                fontSize: '20px',
+                background: 'white',
+                fontWeight: 'bold',
+                borderRadius: '5px',
+                position: 'absolute',
+                bottom: '10px',
+                left: '30px'
+                }} onClick={() => handleFlip(index)}>
+                    Flip</button>
+            </div>
+        </ReactCardFlip>
           </div>
         ))}
+        
       </div>
     );
   }
